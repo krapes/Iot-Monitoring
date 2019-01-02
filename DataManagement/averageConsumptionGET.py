66f28cf57ae8254e7feb2361304b0d8a8a20532d
hizaguirre <heilein.izaguirre@gmail.com>
2019-01-02 11:04:36 -0300

    Adding an operation parameter to control if the user is going to reuqest max or average. The timestep is not taken into account in this implementation The api was not deployed but the local test is working

--- DataManagement/averageConsumptionGET.py
+++ DataManagement/averageConsumptionGET.py
@@ -3,27 +3,42 @@ import boto3
 import json
 import os
 from boto3.dynamodb.conditions import Key
+from json import JSONEncoder
+import decimal
 
 # constants
 global region, stage
 region = os.environ["region"]
 stage = os.environ["stage"]
 
+def date_handler(obj):
+    if hasattr(obj, 'isoformat'):
+        return obj.isoformat()
+    else:
+        if isinstance(obj, decimal.Decimal):
+            return str(obj)
+        else:
+            raise TypeError
+
 def getDataFromDynamo(event):
     # set the client to connect to aws dynamo
-    table_name = "IotStaging"
+    table_name = "IotStaging2"
     dynamodb_resource = boto3.resource('dynamodb',region_name = region)
     table = dynamodb_resource.Table(table_name)
     #@TODO revoew the starDate and endDate values because they are a string and should be a datetime integer
+
+    startDate = int(event["startDate"].replace("-","").replace(":","").replace(" ",""))
+    endDate = int(event["endDate"].replace("-","").replace(":","").replace(" ",""))
     iotStaging = table.query(
-        KeyConditionExpression=Key('IotId').eq(str(event['iotId'])) & Key('datetime').between(str(event["startDate"]), str(event['endDate']))
+        KeyConditionExpression= Key('datetime').between(startDate, endDate) & Key('IotId').eq(str(event['iotId'])),
+        ScanIndexForward = True
     )
 
     # pagination
     while 'LastEvaluatedKey' in iotStaging:
         paged_result = table.query(
-            KeyConditionExpression=Key('IotId').eq(str(event['iotId'])) & Key('datetime').between(
-                str(event["startDate"]), str(event['endDate'])),
+            KeyConditionExpression=Key('IotId').eq(str(event['iotId'])) & Key('datetime').between(startDate, endDate),
+            ScanIndexForward=True,
             ExclusiveStartKey=iotStaging.get('LastEvaluatedKey')
         )
 
@@ -62,10 +77,24 @@ def main(event, context):
        if isValidInput(event):
            records = getDataFromDynamo(event)
 
+           if event['operation'] == "avg":
+               avg =  records["Items"][0]["ADC"] - records["Items"][-1]["ADC"]
+               response = {"code": 200, "data": {"avg":avg}}
+           elif event['operation'] == "max":
+               max = 0
+               time = ""
+               for re in records["Items"]:
+                    if re["ADC"]>max:
+                        max = re["ADC"]
+                        time = str(re["dateStart"])
+               response = {"code": 200, "data": {"max": max,"timestamp":time}}
 
+       else:
+           response = {"code": 400, "data": {"clientError": "Invalid Parameters"}}
 
+       return JSONEncoder(default=date_handler).encode(response)
     except Exception as e:
         print(e)
-        raise Exception(e)
+        return json.dumps({"code": 500, "data": {"serverError": e.message}})
 
 
